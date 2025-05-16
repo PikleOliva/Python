fa2f3cd95483f94db7558fa764cfd12bafb659da
PikleOliva <pikleoliva@hotmail.com>
2025-05-16 13:39:46 -0300

    Update Ej5-4-5.py

--- Ej5-4-5.py
+++ Ej5-4-5.py
@@ -2,19 +2,14 @@ print("-------------------------------------------------------------")
 print("ESCRIBIR LOS ´DÍGITOS QUE INTEGRAN UN NÚMERO ")
 print("-------------------------------------------------------------")
 
-# El fundaento es que a medida que vamos dividiendo por diez, el resto que nos queda el es dígito de la unidad,
-# luego de la decena, etc.
+status_code = 600
 
-num=int(input("Ingresar el número que se quiere descomponer en dígitos "))
-
-while num>0:
-    resto=num%10
-    print (resto)
-    num=num//10
-    
-
-
-
-print("-------------------------------------------------------------")
-print("")
-print("-------------------------------------------------------------")
+match status_code:
+    case 200 as code:
+        print(f"Código de estado: {code} (Ok)")
+    case 400:
+        print("Error del cliente")
+    case 500:
+        print("Error del servidor")
+    case _:
+        print("Código de estado desconocido")
