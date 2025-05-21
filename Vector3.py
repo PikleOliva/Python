707d9e3390b0b93712d69232662bbc7419c260a4
PikleOliva <pikleoliva@hotmail.com>
2025-05-21 09:41:37 -0300

    Update Vector3.py

--- Vector3.py
+++ Vector3.py
@@ -1,34 +1,15 @@
 print("----------------------------------------------------")
-print("DETERMINAR LA TEMPERATURA MEDIA Y LAS QUE LA SUPERAN")
+print("ORDENAR Y QUITAR LOS ELEMENTOS DE UN VECTOR")
 print("----------------------------------------------------")
 
 # Inicializamos las variables un vector vacío
 
-media= 0.000
-suma=0
-cant=int(input("¿Cuántos elementos tendrá el vactor?: "))
-#i=1
-Tempe=[]
-cont=0
-mayor=0
-# Ahora lo llenamos
+Vector=[]
 
-for i in range(cant):
-    
-    valor=float(input("Temperatura: "))
-    Tempe.append(valor)
-    suma=suma+valor
-    cont=cont+1
-media=suma/cont
-for i in range(cant):
-    if Tempe[i]>media:
-        mayor=mayor+1
+tot=int(input("Ingresar el número de elementos a introducir: "))
 
+for i in range(1,tot+1):
+    registro=input("Ingresar el primer registro {0}: ".format(i))
+    Vector.append(registro)
 
-
-# Ahora vemos qué tiene adentro
-
-print("----------------------------------------------------")
-print("La temperatura media fue ",media)
-print("Hubo ",mayor," temperaturas superiores a la media")
-print("----------------------------------------------------")
+print(Vector)
