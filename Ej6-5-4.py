ca6774294e0df3a6aea6c0ba1002b9d218670e6f
PikleOliva <pikleoliva@hotmail.com>
2025-06-01 17:09:42 -0300

    44

--- Ej6-5-4.py
+++ Ej6-5-4.py
@@ -6,32 +6,49 @@ print("Complemento3: INTERACTUAR CON UN VECTOR A TRAVÉS DE OPCIONES")
 print("-------------------------------------------------------")
 #inicializar
 
-Vector = 100*[0]
-i=0
-print(Vector)
 #Llenar el vector con elementos de 1 a 100
 
 tamanio=int(input("Ingresar el tamaño del vector (máx 100): "))
-
-opcion=-1
-for i in range(tamanio): 
-    Vector[i].append(math.trunc(random.random()*100))
-
+Vector = tamanio * [0]
 print (Vector)
- 
-
-while opcion!=0:
-    print("-------------------------------------------------------")
-    print("1 - Ingresar un elemento al vector")
-    print("2 - Eliminar un elemento del vector")
-    print("3 - Listar los elementos del vector")
-    print("4 - Determinar la ocurrencia de un elemento en el vector")
-    print("5 - Calcular la media y el máximo de los elementos del vector")
-    print("0 - Para salir del programa")
-    print("-------------------------------------------------------")
-    opcion= print(input("Elegir una opción (0 para salir del programa):"))
+i=0
 
 for i in range(tamanio): 
-    Vector[i]=input(random.random())*tamanio
-print (Vector)
+   Vector[i]= int(math.trunc(random.random()*100))
     
+print (Vector)
+opcion=-1
+
+print("-----------------------------------")
+print("          M E N Ú    ")
+print("-----------------------------------")
+print("")
+print("1 - Sustituir un elemento al vector")
+print("2 - Eliminar un elemento del vector")
+print("3 - Listar el contenido del vector")
+print("4 - Determinar la repitencia de un elemento dal vector")
+print("5 - Calcular la media y el máximo de un vector")
+print("")
+print("-----------------------------------")
+opcion=int(int(input("Ingresar la opción elegida (0 para salir): ")))
+
+if opcion == 1:
+    i=int(input("Qué registro del vector desea ingresar: "))-1
+    if i<tamanio:
+        elemento=int(input("Qué valor desea asignarle al elemento: "))
+        Vector[i]=elemento
+        print (Vector)
+    else:
+        print("El máximo de registro del vector es de ",tamanio)
+elif opcion == 2:
+    print (Vector)
+    num = int( input("Ingresar el número que se desea eliminar")) 
+    if num > 0: 
+        a = 0 
+#Busca NUM en el array 
+        for j in range(i): 
+            if Vector[j] == num : 
+                a = j
+                print (Vector) 
+                break 
+#Re-organizar el array if a >= 0 and a <= i: for j in range(a, i-1 +1): V[j] = V[j+1] V[i] = 0 i = i - 1      
---
+++ Trabajocon Vectores y menúes.py
@@ -0,0 +1,114 @@
+import math
+import random
+
+print("-------------------------------------------------------")
+print("Complemento3: INTERACTUAR CON UN VECTOR A TRAVÉS DE OPCIONES")
+print("-------------------------------------------------------")
+print("")
+#inicializar
+tamanio=int(input("Ingresar el tamaño del vector (máx 100): "))
+i=0
+Vector=[""]*tamanio
+opcion=-1
+print("-----------------------------------")
+print("          M E N Ú    ")
+print("-----------------------------------")
+print("")
+print("1 - Llenar un vector con elementos nulos")
+print("2 - Llenar un vector con elementos aleatorios")
+print("3 - Llenar un vector con elementos secuenciales (con inicio en 'a' y paso 'b')")
+print("4 - Llenar un vector con elementos selecionados")
+print("5 - Dado un vector, reemplazar uno de sus registros (por posición)")
+print("6 - Dado un vector, reemplazar uno de sus registros (por valor)")
+print("8 - Dado un vector, eliminar uno de sus registros (por valor)")
+print("")
+print("-----------------------------------")
+opcion=int(input("Ingresar la opción elegida (0 para salir): "))
+while opcion!=0:
+    if opcion ==1:
+        print("Llenar un vector con elementos nulos")
+        Vector = tamanio * [0]
+        print (Vector)
+
+    elif opcion ==2:
+        i=0
+        print("Llenar el vector con elementos ale<torios")
+        for i in range(tamanio): 
+            Vector[i]= int(math.trunc(random.random()*100))
+        print (Vector)
+
+    elif opcion==3:
+        print("Llenar el vector con elementos secuenciales (empezando con a)")
+        a=int(input("¿Con qué número querés empezar?: "))
+        b=int(input("¿Qué paso querés que tenga el vector?: "))
+        for i in range(tamanio):
+            Vector[i]=int(a)
+            a=a+b
+        print(Vector)
+
+    elif opcion==4:
+        print("Llenar el vector con elementos seleccionados)")
+        for i in range(tamanio):
+            Vector[i]=int(input("ingresar el elemento: "))
+        print (Vector)
+
+    elif opcion == 5:
+        i=0
+        for i in range(tamanio): 
+            Vector[i]= int(math.trunc(random.random()*100))
+        print (Vector)
+        i=int(input("¿Qué registro del vector desea reemplazar?: "))-1
+        if i<tamanio:
+            elemento=int(input("Qué valor desea asignarle al elemento: "))
+            Vector[i]=elemento
+            print (Vector)
+    elif opcion == 6:
+        i=0
+        for i in range(tamanio): 
+            Vector[i]= int(math.trunc(random.random()*100))
+        print (Vector)
+        valor=int(input("¿Qué valor del vector desea reemplazar?: "))
+        if valor in Vector:
+            i=Vector.index(valor)
+            if i<tamanio:
+                elemento=int(input("Qué valor desea asignarle al elemento: "))
+                Vector[i]=elemento
+            print (Vector)
+
+    elif opcion==7:
+        Copia=(tamanio-1)*['']
+        print("Llenar el vector con elementos seleccionados)")
+        for i in range(tamanio):
+            Vector[i]=int(input("Ingresar el elemento: "))
+        print ("El vector ingresado es ",Vector)
+        reg=int(input("¿Qué registro del vector desea eliminar?: "))-1
+    
+        i=0
+        while i<reg:
+            for i in range(tamanio-1):
+                Copia[i]=Vector[i]
+                i+1
+        for i in range(reg,tamanio-1):
+            Copia[i]=Vector[i+1]
+        Vector=Copia
+        print("El vector reducido es ",Vector)
+
+    elif opcion==8:
+        Copia=(tamanio-1)*['']
+        print("Llenar el vector con elementos seleccionados)")
+        for i in range(tamanio):
+            Vector[i]=int(input("Ingresar el elemento: "))
+        print ("El vector ingresado es ",Vector)
+        valor=int(input("¿Qué valor del vector desea elimninar?: "))
+        if valor in Vector:
+            reg=Vector.index(valor)
+        i=0
+        while i<reg:
+            for i in range(tamanio-1):
+                Copia[i]=Vector[i]
+                i+1
+        for i in range(reg,tamanio-1):
+            Copia[i]=Vector[i+1]
+        Vector=Copia
+        print("El vector reducido es ",Vector)
+    
