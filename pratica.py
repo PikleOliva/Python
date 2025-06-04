1a0f010494b2f27838f3e16b9c9001b3e1e6dc0e
PikleOliva <pikleoliva@hotmail.com>
2025-06-03 22:16:23 -0300

    11

--- pratica.py
+++ pratica.py
@@ -1,13 +1,23 @@
 import math
 import random
-alto=int(input("Filas: "))
-ancho=int(input("Columnas: "))
-Matriz=[]
-for i in range(alto):
-    fila=[]
-    for j in range(ancho):
-        elemento= int(input(f"Elemento de la fila {i+1}: "))
-        fila.append(elemento)
-    Matriz.append(fila)
-for fila in Matriz:
-    print (fila)
+largo=7
+Vector=[]
+max=0
+suma=0
+contador=0
+i=0
+for i in range(largo):
+    elemento=math.trunc(random.random()*100)
+    Vector.append(int(elemento))
+print (Vector)
+
+suma=sum(Vector)
+print (suma)
+prome=suma/largo
+print(prome)
+
+ordenado=Vector.sort()
+print(ordenado)
+
+reducido=Vector.pop()
+print (reducido)
