cd7d173fab30649941d1ce8a8e86c4e69a52c296
PikleOliva <pikleoliva@hotmail.com>
2025-06-27 10:53:04 -0300

    hhh

--- TPI_1.py
+++ TPI_1.py
@@ -2,23 +2,30 @@ print("----------------------------------------")
 print("CALCULADORA DE GASTOS MENSUALES")
 print("----------------------------------------")
 
-presu=100000
-gasto=1
+presu=int(input("El presupuesto disponible es de $ "))
 consumo=0
-print("El presupuesto disponible es de $ ",presu)
-while gasto!=0 and consumo<presu:
 
-    gasto=int(input("Ingresar el nuevo gasto: "))
+def funcion():
+    consumo=int(input("Ingresar nuievo gasto: "))
+    return consumo
+
+while consumo<presu:
+    print(consumo)
     anterior=consumo
-    consumo=consumo+gasto
-    if consumo>presu:
-        print("----------------------------------------")
-        print("Los gastos han excedido el presupuesto disponible")
-        # print("Los gastos mensuales han sido de $ ",anterior)
-        print("----------------------------------------")
-        consumo=anterior
-        gasto=0
+    funcion()
+    print(consumo)
+
+#     gasto=int(input("Ingresar el nuevo gasto: "))
+#     anterior=consumo
+#     consumo=consumo+gasto
+#     if consumo>presu:
+#         print("----------------------------------------")
+#         print("Los gastos han excedido el presupuesto disponible")
+#         # print("Los gastos mensuales han sido de $ ",anterior)
+#         print("----------------------------------------")
+#         consumo=anterior
+#         gasto=0
     
-print("----------------------------------------")   
-print("Los gastos mensuales han sido de $ ",consumo)
-print("----------------------------------------")   
+# print("----------------------------------------")   
+# print("Los gastos mensuales han sido de $ ",consumo)
+# print("----------------------------------------")   
---
+++ auxiliar.py
@@ -0,0 +1,86 @@
+# def dibujarRayas():
+#     for i in range(40):
+#         print("-", end=(" "))
+
+# def sumar(a, b):
+#     resultado=a+b
+#     return resultado
+# def restar(a, b):
+#     resultado=a-b
+#     return resultado
+# def multipli(a, b):
+#     resultado=a*b
+#     return resultado
+# def divide(a, b):
+#     resultado=a/b
+#     return resultado
+# def potencia(a,b):
+#     resultado=a**b
+#     return resultado
+# def raiz(a,b):
+#     resultado=a**(1/b)
+#     return resultado
+
+# a= int(input("Ingresar primer número: "))
+# b= int(input("Ingresar el otro número: "))
+# resultado=float
+# dibujarRayas
+# resultado=sumar(a,b)
+# print (resultado)
+# dibujarRayas
+# resultado=restar(a,b)
+# print (resultado)
+# dibujarRayas
+# resultado=multipli(a,b)
+# print (resultado)
+# dibujarRayas
+# resultado=round(divide(a,b),4)
+# print (resultado)
+# dibujarRayas
+# resultado=potencia(a,b)
+# print (resultado)
+# dibujarRayas
+# resultado=round(raiz(a,b),4)
+# print (resultado)
+# dibujarRayas
+# print()
+# for i in range(1,6):
+#     resultado=sumar(i,i+1)
+#     print (resultado)
+# print()
+# for i in range(1,6):
+#     resultado=multipli(7,i)
+#     print(resultado)
+
+# arreglo=[]
+# for i in range (4):
+#     arreglo.append(int(input("Ingresar un número: ")))
+    
+# print(arreglo)
+# for i in range(4):
+#     print(f"El número de índice {i} es {arreglo[i]}")
+
+# arreglo=[]
+# for i in range(4):
+#     arreglo.append(input("Ingresar una palabra: "))
+# print (arreglo)
+# arreglo=[]
+
+# for i in range (3):
+#     arreglo.append([])
+#     for j in range(2):
+#         arreglo[i].append(int(input(f"Ingresar el registro {i},{j}: ")))
+    
+#print (arreglo)
+# for i in range(3):
+    # print(arreglo[i])
+
+# arreglo=[]
+# largo=int(input("Ingresar la longitud del arreglo: "))
+# for i in range(largo):
+#     arreglo.append(input(f"Ingresar el nombre de la posición {i+1}: "))
+    
+# for i in range(largo):
+#     print (arreglo[i]) 
+# print (arreglo)
+
